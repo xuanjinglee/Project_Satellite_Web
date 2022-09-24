9542deac3d1460743599b8ce6dab97f42e78bda4
xuanjing lee <xuanjinglee@gmail.com>
2022-09-24 16:50:43 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import cv2
-import numpy as np
 import requests
 
 '''
@@ -25,7 +23,7 @@ if uploaded_image is not None:
 
     # Call API to do prediction
 
-    api_url = "https://segapi-tnfnn6u4nq-ew.a.run.app/upload_image"
+    api_url = "https://sigapi-tnfnn6u4nq-ew.a.run.app/"
 
     headers = {
         'accept': 'application/json',
@@ -36,15 +34,36 @@ if uploaded_image is not None:
         'file': uploaded_image,
     }
     # response = requests.post('http://127.0.0.1:8000/upload', headers=headers, files=files)
-    response = requests.post('https://segapi-tnfnn6u4nq-ew.a.run.app/upload_image', headers=headers, files=files)
+    response = requests.post(api_url + 'upload_image', headers=headers, files=files)
 
-    st.write(response.text)
+    if response.text == f'{{"message":"Successfully uploaded {uploaded_image.name}"}}':
+        st.success(f'Successfully uploaded {uploaded_image.name}')
 
-    params = {
-        'filename': f'transformed_{uploaded_image.name}',
-    }
-    response = requests.get('https://segapi-tnfnn6u4nq-ew.a.run.app/get_image', params=params, headers=headers)
-    # from IPython.display import Image, display
-    # display(Image(response.content))
 
-    st.image(response.content)
+    col1, col2 = st.columns(2)
+
+    with col1:
+        params = {
+            'filename': f't1_{uploaded_image.name}',
+        }
+        response = requests.get(api_url +'get_image', params=params, headers=headers)
+        # from IPython.display import Image, display
+        # display(Image(response.content))
+
+        # if response.status_code != 200:
+        #     params = {'filename': "owl.jpg",}
+        #     response = requests.get(api_url +'get_image', params=params, headers=headers)
+
+        st.image(response.content)
+        st.caption("<h4 style='text-align: center; color: grey;'>Building, Land, Road, Vegetation, Water and Others</h4>", unsafe_allow_html=True)
+
+    with col2:
+        params = {
+            'filename': f't2_{uploaded_image.name}',
+        }
+        response = requests.get(api_url +'get_image', params=params, headers=headers)
+        # from IPython.display import Image, display
+        # display(Image(response.content))
+
+        st.image(response.content)
+        st.caption("<h4 style='text-align: center; color: grey;'>Buildings only</h4>", unsafe_allow_html=True)
